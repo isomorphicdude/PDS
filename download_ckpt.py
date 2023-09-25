247e6df09c61d2d486d336258dabc1bfdd4268aa
isomorphicdude <iamthesenate@qq.com>
2023-09-25 10:43:30 +0100

    update download

--- download_ckpt.py
+++ download_ckpt.py
@@ -1,12 +1,16 @@
 import os
 import gdown
 import tensorflow as tf
+from absl import flags
 
 # !mkdir checkpoints
 # !mkdir masks
 
-tf.io.gfile.makedirs('/content/PDS/checkpoints')
-tf.io.gfile.makedirs('/content/PDS/masks')
+flags.DEFINE_string("workdir", None, "Work directory.")
+FLAGS = flags.FLAGS
+
+tf.io.gfile.makedirs(os.path.join(FLAGS.workdir, 'checkpoints'))
+tf.io.gfile.makedirs(os.path.join(FLAGS.workdir, 'masks'))
 
 cwd = os.getcwd()
 url_24 = 'https://drive.google.com/uc?id=1JInV8bPGy18QiIzZcS1iECGHCuXL6_Nz'
@@ -15,13 +19,11 @@ url_12 = 'https://drive.google.com/uc?id=1yS8QZb_6tCeZkY7DK4_RI-Crc6LQLILN' # de
 
 url_mask_cifar10 = 'https://drive.google.com/uc?id=1tz-ObG-WRFGNFDxnJW6YgtzhKYhDWBS9'
 
-gdown.download(url_24, '/content/PDS/checkpoints/checkpoint_24.pth', quiet=False)
-gdown.download(url_8, '/content/PDS/checkpoints/checkpoint_8.pth', quiet=False)
-gdown.download(url_12, '/content/PDS/checkpoints/checkpoint_12.pth', quiet=False)
+gdown.download(url_24, os.path.join(FLAGS.workdir, 'checkpoints', 'checkpoint_24.pth'), quiet=False)
+gdown.download(url_8, os.path.join(FLAGS.workdir, 'checkpoints', 'checkpoint_8.pth'), quiet=False)
+gdown.download(url_12, os.path.join(FLAGS.workdir, 'checkpoints', 'checkpoint_12.pth'), quiet=False)
+gdown.download(url_mask_cifar10, os.path.join(FLAGS.workdir, 'masks', 'cifar10_freq'), quiet=False)
 
-gdown.download(url_mask_cifar10, '/content/PDS/masks/cifar10_freq', quiet=False)
-
-
-tf.io.gfile.makedirs('/content/PDS/assets/stats')
+tf.io.gfile.makedirs(os.path.join(FLAGS.workdir, 'assets', 'stats'))
 url_stats_cifar10 = 'https://drive.google.com/uc?id=14UB27-Spi8VjZYKST3ZcT8YVhAluiFWI'
-gdown.download(url_stats_cifar10, '/content/PDS/assets/stats/cifar10_stats.npz', quiet=False)
+gdown.download(url_stats_cifar10, os.path.join(FLAGS.workdir, 'assets', 'stats', 'cifar10_stats.npz'), quiet=False)
